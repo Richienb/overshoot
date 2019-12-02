b8d713427094d429796c04dc45aed455f1f02189
Richie Bendall <richiebendall@gmail.com>
2019-12-02 16:03:29 +1300

    refactor: Replace got with ky
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 "use strict"
 
-const got = require("got")
 const { load } = require("cheerio")
-const toBluebird = require("to-bluebird")
 const _ = require("lodash")
 const day = require("dayjs")
+const ky = require("ky-universal")
 
-module.exports = () => toBluebird(got("https://www.overshootday.org/newsroom/past-earth-overshoot-days/"))
-    .then(({ body }) => load(body))
+module.exports = () => ky("https://www.overshootday.org/newsroom/past-earth-overshoot-days/")
+    .then((res) => res.text())
+    .then((body) => load(body))
     .then(($) => $("div.col.span_12.dark.left > div.vc_col-sm-4.wpb_column.column_container.vc_column_container.col.no-extra-padding > div > div > div > div > p"))
     .then((els) => els.last())
     .then((el) => el.text())
